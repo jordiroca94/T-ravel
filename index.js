136f7cdfde9ec0ed716d80031cba1ae781a55212
Jordi Roca <jordirocasoler94@gmail.com>
2022-02-24 20:01:15 +0100

    create a pin route

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json())
+
 mongoose
   .connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology:true })
   .then(() => {
